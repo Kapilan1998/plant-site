a1ca090ab698995cea2302abd8b78730af9463c3
kapilan <sriranjankapilan@gmail.com>
2025-03-13 20:41:10 +0530

    merge together

---
+++ main.js
@@ -0,0 +1,29 @@
+/*~~~~~~~~~~~~~~~ TOGGLE BUTTON ~~~~~~~~~~~~~~~*/
+let navMenu = document.getElementById("nav-menu")
+let navLink = document.querySelectorAll(".nav-link")
+let hamburger = document.getElementById("hamburger")
+
+hamburger.addEventListener("click",()=>{
+    navMenu.classList.toggle("left-[0]")
+    hamburger.classList.toggle('ri-close-large-fill')
+})
+
+navLink.forEach(link =>{
+    link.addEventListener("click",() =>{
+        navMenu.classList.toggle("left-[0]");
+        hamburger.classList.toggle('ri-close-large-fill');
+    })
+})
+/*~~~~~~~~~~~~~~~ SHOW SCROLL UP ~~~~~~~~~~~~~~~*/
+
+
+/*~~~~~~~~~~~~~~~ CHANGE BACKGROUND HEADER ~~~~~~~~~~~~~~~*/
+
+
+/*~~~~~~~~~~~~~~~ SWIPER ~~~~~~~~~~~~~~~*/
+
+
+/*~~~~~~~~~~~~~~~ SCROLL SECTIONS ACTIVE LINK ~~~~~~~~~~~~~~~*/
+
+
+/*~~~~~~~~~~~~~~~ SCROLL REVEAL ANIMATION ~~~~~~~~~~~~~~~*/
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,29 @@ export default {
       xl: "1180px"
     },
     extend: {},
+    keyframes: {
+      move: {
+        "50%": { transform: "translateY(-1rem)" }
+      },
+      rotate : {
+        "0%": { transform: "rotate(0deg)" },
+        "100%": { transform: "rotate(360deg)" }
+      },
+      scaleUp : {
+        "0%": { transform: "scale(0.8)" },
+        "50%": { transform: "scale(1.2)" },
+        "100%": { transform: "scale(0.8)"}
+      }
+    },
+    animation: {
+      movingY: "move 3s linear infinite",
+      rotating: "rotate 10s linear infinite",
+      scalingUp: "scaleUp 3s linear infinite"
+    },
     fontFamily: {
       Jost: ["Jost", "sans-serif"],
       Lobster: ["Lobster", "sans-serif"],
-      Emblema:["Emblema One", "system-ui"]
+      Emblema: ["Emblema One", "system-ui"]
     },
     container: {
       center: true,
