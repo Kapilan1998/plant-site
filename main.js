56b4135322f24a20231857616464ae52964b2390
kapilan <sriranjankapilan@gmail.com>
2025-03-16 12:43:47 +0530

    footer added

--- main.js
+++ main.js
@@ -3,54 +3,117 @@ let navMenu = document.getElementById("nav-menu")
 let navLink = document.querySelectorAll(".nav-link")
 let hamburger = document.getElementById("hamburger")
 
-hamburger.addEventListener("click",()=>{
-    navMenu.classList.toggle("left-[0]")
-    hamburger.classList.toggle('ri-close-large-fill')
+hamburger.addEventListener("click", () => {
+  navMenu.classList.toggle("left-[0]")
+  hamburger.classList.toggle('ri-close-large-fill')
 })
 
-navLink.forEach(link =>{
-    link.addEventListener("click",() =>{
-        navMenu.classList.toggle("left-[0]");
-        hamburger.classList.toggle('ri-close-large-fill');
-    })
+navLink.forEach(link => {
+  link.addEventListener("click", () => {
+    navMenu.classList.toggle("left-[0]");
+    hamburger.classList.toggle('ri-close-large-fill');
+  })
 })
 
 /*~~~~~~~~~~~~~~~ SHOW SCROLL UP ~~~~~~~~~~~~~~~*/
+let scrollUp = () => {
+  let scrollUpBtn = document.getElementById("scroll-up")
 
+  if (this.scrollY >= 250) {
+    scrollUpBtn.classList.remove("-bottom-1/2")
+    scrollUpBtn.classList.add("bottom-4")
+  } else {
+    scrollUpBtn.classList.add("-bottom-1/2")
+    scrollUpBtn.classList.remove("bottom-4")
+  }
+}
+
+window.addEventListener("scroll", scrollUp);
 
 /*~~~~~~~~~~~~~~~ CHANGE BACKGROUND HEADER ~~~~~~~~~~~~~~~*/
+let scrollHeader = () => {
+  let HEADER = document.getElementById("navbar")
+
+  if (this.scrollY >= 100) {
+    HEADER.classList.add("border-b", "border-yellow-500")
+  } else {
+    HEADER.classList.remove("border-b", "border-yellow-500")
+  }
+}
+
+window.addEventListener("scroll", scrollHeader);
 
 
 /*~~~~~~~~~~~~~~~ SWIPER ~~~~~~~~~~~~~~~*/
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-  speed:400,
-  spaceBetween:30,
-  autoplay:{
-    delay:3000,
-    disableOnInteraction:false
+  // Optional parameters
+  speed: 400,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false
   },
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable:true
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  grabCursor: true,
+  breakpoints: {
+    640: {
+      slidesPerView: 1
     },
-    grabCursor:true,
-    breakpoints:{
-        640:{
-            slidesPerView:1
-        },
-        768:{
-            slidesPerView:2
-        },
-        1024:{
-            slidesPerView:3
-        },
-    }
-  });
+    768: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    },
+  }
+});
 
 /*~~~~~~~~~~~~~~~ SCROLL SECTIONS ACTIVE LINK ~~~~~~~~~~~~~~~*/
+let activeLink = () => {
+  let sections = document.querySelectorAll('section')
+  let navLinks = document.querySelectorAll(".nav-link")
 
+  let current = "home"
+  sections.forEach(section => {
+    let sectionTop = section.offsetTop;
+
+    if (this.scrollY >= sectionTop - 60) {
+      current = section.getAttribute("id")
+    }
+  })
+
+  navLinks.forEach(item => {
+    item.classList.remove("active")
+
+    if(item.href.includes(current)){
+      item.classList.add("active")
+    }
+  })
+}
+
+window.addEventListener("scroll",activeLink);
 
 /*~~~~~~~~~~~~~~~ SCROLL REVEAL ANIMATION ~~~~~~~~~~~~~~~*/
+let sr = ScrollReveal({
+  origin:"top",
+  distance:"60px",
+  duration:2500,
+  delay:300,
+  reset:true
+})
+
+sr.reveal('.home__data, .about__top, .popular__top, .review__top, .review__swiper, .footer__icon, footer__content, .copy__right')
+sr.reveal('.home__image',{delay:500,scale:0.5})
+
+sr.reveal('.service__card, .popular__card',{interval:100})
+
+sr.reveal('.about__leaf',{delay:1000,origin:"left"})
+sr.reveal('.about__item__1-content, .about__item__2-img',{origin:"right"})
+sr.reveal('.about__item__2-content, .about__item__1-img',{origin:"left"})
+
+sr.reveal('.review__leaf, .footer__floral',{delay:1000,origin:"left"})
